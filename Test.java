6533809e10afa5a045384260d25b7c7ff47869a6
Cuathimbong <egvika20051@gmail.com>
2025-09-17 01:59:42 +0300

    First test.

---
+++ Test.java
@@ -0,0 +1,31 @@
+public class Test {
+    public static int addDigits(char[] a) {
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            int digit = Character.getNumericValue(a[i]);
+            sum += digit;
+        }
+
+        return sum;
+    }
+
+    public static String turnToString(int m) {
+        String mStr = Integer.toString(m);
+        return mStr;
+    }
+
+    public static void main(String[] args) {
+        char[] argsArray = args[0].toCharArray();
+
+        while (true) {
+            int sum = addDigits(argsArray);
+            argsArray = turnToString(sum).toCharArray();
+            if (argsArray.length == 1) {
+                break;
+            }
+        }
+
+        System.out.println(argsArray);
+    }
+}
